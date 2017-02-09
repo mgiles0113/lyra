4e736cb9b2e57fefca1ec14f1ca898f3df643a1f
giles <giles.1q@gmail.com>
2017-02-09 17:32:12 +0000

    Corinna: added loading doors from new files

--- js/game/lyra/door.js
+++ js/game/lyra/door.js
@@ -1,17 +1,59 @@
 class Door {
+    constructor (idx) {
+        this.idx = idx;
+    }
+    
     addDoor (game, name, x, y) {
     //Test to see if it works.
-        this.sprite = game.add.tileSprite(x, y, 64, 64, 'dooropen');
+        for (var i=0; i< game.gameData.doors.length; i++) {
+            this.sprite = game.add.tileSprite(x, y, game.gameData.doors[i].width, game.gameData.doors[i].height, game.gameData.doors[i].imageTagList);
+        }
         this.name = name;
+        this.state = game.gameData.doors[0].imageTagList;
         game.physics.arcade.enable(this.sprite);
-        this.sprite.body.setSize(64, 64);
-        // this.sprite.anchor.set(0.5, 0.5); // center collision over image
+        this.sprite.body.setSize(game.gameData.doors[0].width, game.gameData.doors[0].height);
+    }
+    
+    
+    // [TODO] improve mapping here to correspond door states to the input
+    openDoor (game) {
+        this.state = game.gameData.doors[0].imageTagList;
+        this.sprite.body.setSize(game.gameData.doors[0].width, game.gameData.doors[0].height);
+    }
+
+    closedDoor (game) {
+        this.state = game.gameData.doors[1].imageTagList;
+        this.sprite.body.setSize(game.gameData.doors[1].width, game.gameData.doors[1].height);
+    }
+    
+    openDoorHighlighted (game) {
+        this.state = game.gameData.doors[2].imageTagList;
+        this.sprite.body.setSize(game.gameData.doors[2].width, game.gameData.doors[2].height);
+    }   
+
+    closedDoorHighlighted (game) {
+        this.state = game.gameData.doors[3].imageTagList;
+        this.sprite.body.setSize(game.gameData.doors[3].width, game.gameData.doors[3].height);
+    }
+
+    saveDoor (offsetX, offsetY) {
+        var doorData = {
+            idx : this.idx,
+            name: this.name,
+            state: this.state,
+            x : this.sprite.body.position.x + offsetX,
+            y : this.sprite.body.position.y + offsetY,
+        }
+        return (doorData);
     }
 }
 
+
 Door.preloadDoorImages = function(game) {
-    game.load.image('dooropen', 'assets/lyraImages/dooropen.png');
-    game.load.image('doorclosed', 'assets/lyraImages/doorclosed.png');
-    game.load.image('dooropenhighlighted', 'assets/lyraImages/dooropenhighlighted.png');
-    game.load.image('doorclosedhighlighted', 'assets/lyraImages/doorclosedhighlighted.png');
+    // load all the specified door images
+    for (var i=0; i< game.gameData.doors.length; i++) {
+        game.load.image(game.gameData.doors[i].imageTagList,game.gameData.doors[i].imageRefList)
+    }
 }
+
+
